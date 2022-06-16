b16a095a521d9262b46535c1ddcdd86637427652
Lior Chamla <lchamla@gmail.com>
2022-06-16 00:45:37 +0000

    fixing compile script for heroku deployment

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
