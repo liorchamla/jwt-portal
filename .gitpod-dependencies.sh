b244b3deb857be5c4567bfc3f3264d86a08675da
Lior Chamla <lchamla@gmail.com>
2022-06-17 21:06:08 +0000

    build: added a script to gitpod configuration for gitflow

---
+++ .gitpod-dependencies.sh
@@ -0,0 +1,4 @@
+sudo apt-get install git-flow
+sudo apt-get install bash-completion
+git clone https://github.com/bobthecow/git-flow-completion.git ~/.git-flow-completion
+sudo ln -s ~/.git-flow-completion/git-flow-completion.bash /etc/bash_completion.d/git-flow-completion.bash
